497bb0473bd7add4e64b90e45bb9cab6f9e5c185
QayyumUrfa <qayyumurfamedicalstore@gmail.com>
2025-07-20 03:07:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function toggleMenu() {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('show');
-}
 
-function validateForm() {
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  const message = document.getElementById('message');
-
-  if (!name.value || !email.value || !message.value) {
-    alert("Please complete all fields.");
-    return false;
-  }
-
-  alert("Thank you for contacting us. We will get back to you shortly.");
-  return true;
-}
